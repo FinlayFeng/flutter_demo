c978a6bb40381a83a1aa5d6e0facddbbcaea3d50
fengli <15255116418@163.com>
2020-08-27 12:31:19 +0800

    增加dio

--- lib/chapter2.dart
+++ lib/chapter2.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,6 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
 class Chapter2 extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     List<Widget> list = [
@@ -18,6 +18,7 @@ class Chapter2 extends StatelessWidget {
       ProviderDemo(),
       HttpDemo(),
       HttpClientDemo(),
+      DioDemo(),
     ];
     return Scaffold(
       appBar: AppBar(
@@ -65,6 +66,7 @@ class ThemeDemo extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   //标题
   final String title;
+
   //接收title值 key为widget的唯一标识
   MyHomePage({Key key, @required this.title}) : super(key: key);
 
@@ -101,20 +103,20 @@ class PackageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('使用第三方包示例'),
+      appBar: AppBar(
+        title: Text('使用第三方包示例'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //指定url并发起请求
+            const url = 'https://github.com';
+            //调用url_launcher包里的launch方法
+            launch(url);
+          },
+          child: Text('打开GitHub'),
         ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {
-              //指定url并发起请求
-              const url = 'https://github.com';
-              //调用url_launcher包里的launch方法
-              launch(url);
-            },
-            child: Text('打开GitHub'),
-          ),
-        ),
+      ),
     );
   }
 }
@@ -131,6 +133,7 @@ class StateDemo extends StatelessWidget {
 //主页需要继承自StatefulWidget
 class NewHomePage extends StatefulWidget {
   NewHomePage({Key key, this.title}) : super(key: key);
+
   //标题
   final String title;
 
@@ -141,7 +144,7 @@ class NewHomePage extends StatefulWidget {
 
 //状态类必须继承State类,注意后面需要指定为<MyHomePage>
 class _MyHomePageState extends State<NewHomePage> {
-  int _counter = 0;//计数器
+  int _counter = 0; //计数器
 
   void _incrementCounter() {
     //调用State类里的setState方法来更改状态值，使得计数器加1
@@ -153,14 +156,12 @@ class _MyHomePageState extends State<NewHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       //居中布局
       body: Center(
-
         //垂直布局
         child: Column(
           //主轴居中对齐
@@ -170,14 +171,14 @@ class _MyHomePageState extends State<NewHomePage> {
               '你点击右下角按钮的次数:',
             ),
             Text(
-              '$_counter',//绑定计数器的值
+              '$_counter', //绑定计数器的值
               style: Theme.of(context).textTheme.bodyText1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,//按下+号按钮调用自增函数
+        onPressed: _incrementCounter, //按下+号按钮调用自增函数
         tooltip: '增加',
         child: Icon(Icons.add),
       ),
@@ -190,14 +191,10 @@ class ProviderDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     //使用MultiProvider可以创建多个顶层共享数据
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => Counter())
-      ],
+      providers: [ChangeNotifierProvider(create: (context) => Counter())],
       child: MaterialApp(
         title: "Provider示例",
-        theme: ThemeData(
-
-        ),
+        theme: ThemeData(),
         darkTheme: ThemeData(
           brightness: Brightness.dark,
         ),
@@ -220,7 +217,8 @@ class FirstPage extends StatelessWidget {
             //路由跳转至第二页
             onPressed: () =>
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SecondPage();})),
+              return SecondPage();
+            })),
           )
         ],
       ),
@@ -242,22 +240,22 @@ class FirstPage extends StatelessWidget {
 //第二个页面
 class SecondPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("第二个页面"),
-        ),
-        body: Center(
-          //获取计数器中的count值
-          child: Text("${Provider.of<Counter>(context).count}"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //调用数据模型中的increment方法更改数据
-            Provider.of<Counter>(context, listen: false).increment();
-          },
-          child: Icon(Icons.add),
-        ),
+      appBar: AppBar(
+        title: Text("第二个页面"),
+      ),
+      body: Center(
+        //获取计数器中的count值
+        child: Text("${Provider.of<Counter>(context).count}"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //调用数据模型中的increment方法更改数据
+          Provider.of<Counter>(context, listen: false).increment();
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -267,14 +265,14 @@ class SecondPage extends StatelessWidget {
 /// 使用mixin混入ChangeNotifier类，这个类能够自动管理所有听众。
 /// 当调用notifyListeners时，它会通知所有听众进行刷新。
 class Counter with ChangeNotifier {
-
   //存储数据
   int _count = 0;
+
   //提供外部能够访问的数据
   int get count => _count;
 
   //提供更改数据的方法
-  void increment(){
+  void increment() {
     _count++;
     //通知所有听众进行刷新
     notifyListeners();
@@ -285,29 +283,28 @@ class HttpDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('http请求示例'),
+      appBar: AppBar(
+        title: Text('http请求示例'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //请求后台url路径(IP + PORT + 请求接口)
+            var url = 'https://www.wanandroid.com/article/list/0/json';
+            //向后台发起get请求 response为返回对象
+            http.get(url).then((response) {
+              print("状态： ${response.statusCode}");
+              print("正文： ${response.body}");
+            });
+          },
+          child: Text('发起http请求'),
         ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {
-              //请求后台url路径(IP + PORT + 请求接口)
-              var url = 'https://www.wanandroid.com/article/list/0/json';
-              //向后台发起get请求 response为返回对象
-              http.get(url).then((response) {
-                print("状态： ${response.statusCode}");
-                print("正文： ${response.body}");
-              });
-            },
-            child: Text('发起http请求'),
-          ),
-        ),
+      ),
     );
   }
 }
 
 class HttpClientDemo extends StatelessWidget {
-
   //获取数据 此方法需要异步执行async/await
   void getHttpClientData() async {
     try {
@@ -315,8 +312,8 @@ class HttpClientDemo extends StatelessWidget {
       HttpClient httpClient = HttpClient();
 
       //发起请求
-      HttpClientRequest request = await httpClient.getUrl(
-          Uri.parse("https://www.wanandroid.com/article/list/0/json"));
+      HttpClientRequest request = await httpClient
+          .getUrl(Uri.parse("https://www.wanandroid.com/article/list/0/json"));
 
       //等待服务器返回数据
       HttpClientResponse response = await request.close();
@@ -328,26 +325,43 @@ class HttpClientDemo extends StatelessWidget {
 
       //httpClient关闭
       httpClient.close();
-
     } catch (e) {
       print("请求失败：$e");
-    } finally {
-
-    }
+    } finally {}
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('HttpClient请求'),
+      appBar: AppBar(
+        title: Text('HttpClient请求'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("发起HttpClient请求"),
+          onPressed: getHttpClientData,
         ),
-        body: Center(
+      ),
+    );
+  }
+}
+
+class DioDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("dio示例")),
+      body: Center(
           child: RaisedButton(
-            child: Text("发起HttpClient请求"),
-            onPressed: getHttpClientData,
-          ),
-        ),
+              onPressed: () async {
+                try {
+                  Response response = await Dio().get("http://www.baidu.com");
+                  print(response.statusCode);
+                } catch (e) {
+                  print(e);
+                }
+              },
+              child: Text("使用dio请求百度"))),
     );
   }
 }
